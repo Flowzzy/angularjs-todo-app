24def0bbd918ae545174c8eddf33901bf5f5b8fa
flowzzy <frenques.sitoe@gmail.com>
2018-10-12 11:01:50 +0200

    schema update

--- db_schema/db_schema.sql
+++ db_schema/db_schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE todos (
   id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(50),
-  dueDate varchar(50),
+  complete BOOLEAN,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
-INSERT INTO todos (id, name, dueDate) VALUES
-(1, 'Create AngularApp', '10/4/2018'),
-(2, 'Create database', '10/3/2018'),
-(3, 'Push app to GitHub', '10/4/2018'),
-(4, 'Review code', '10/3/2018');
+INSERT INTO todos (id, name, complete) VALUES
+(1, 'Create AngularApp', 0),
+(2, 'Create database', 0),
+(3, 'Push app to GitHub', 0),
+(4, 'Review code', 0);
