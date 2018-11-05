b65b7977fae13f5f4b81d7f00f85a3b4b038611a
flowzzy <flowzzyws@gmail.com>
2018-11-05 09:04:56 +0200

    Code restructure

---
+++ app/controllers/todoController.js
@@ -0,0 +1,47 @@
+
+
+angular.module('todoApp', [])
+  .controller('TodoListController', function ($scope, $http) {
+
+    var url = "/todos";
+
+    var getTodos = function () {
+      $http.get(url).then((response) => {
+        $scope.todoList = response.data;
+      }, (response) => {
+        console.log(response);
+        $scope.getErrMsg = "Error Status: " + response;
+      });
+    }
+
+    $scope.addTodo = function () {
+      $http.post("/addTodo", { "name": $scope.newTodo, "complete": "0" }).then(function (data, status) {
+        $scope.newTodo = "";
+        getTodos();
+      })
+    }
+
+    $scope.completeTodo = function (todo) {
+
+      $http.put("/completeTodo/"+todo.id).then(function (data, status) {
+        getTodos();
+      })
+    }
+
+    $scope.removeItem = function (todo) {
+      $http.delete("/deleteTodo/"+todo.id).then(function (data, status) {
+        getTodos();
+      })
+    }
+
+    getTodos();
+
+    $scope.$watch(function(){
+      return $scope.checkbox;
+  }, function(){
+      $scope.checkbox = Number($scope.checkbox);
+      console.log($scope.checkbox, typeof $scope.checkbox);
+  },true);
+
+  });
+
---
+++ resources/db_schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE todoDB;
+
+CREATE TABLE todos (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(50),
+  complete BOOLEAN,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+INSERT INTO todos (id, name, complete) VALUES
+(1, 'Create AngularApp', 0),
+(2, 'Create database', 0),
+(3, 'Push app to GitHub', 0),
+(4, 'Review code', 0);
