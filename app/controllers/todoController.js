ed2259ef7e909c830f0d7e7ba0e4c21e06780833
flowzzy <flowzzyws@gmail.com>
2018-11-05 15:45:58 +0200

    Todo list view directive adition

--- app/controllers/todoController.js
+++ app/controllers/todoController.js
@@ -1,47 +1,60 @@
 
 
-angular.module('todoApp', [])
-  .controller('TodoListController', function ($scope, $http) {
+var myApp = angular.module('todoApp', []);
 
-    var url = "/todos";
+myApp.controller('TodoListController', function ($scope, $http) {
 
-    var getTodos = function () {
-      $http.get(url).then((response) => {
-        $scope.todoList = response.data;
-      }, (response) => {
-        console.log(response);
-        $scope.getErrMsg = "Error Status: " + response;
-      });
+  var url = "/todos";
+
+  var getTodos = function () {
+    $http.get(url).then((response) => {
+      $scope.todoList = response.data;
+    }, (response) => {
+      console.log(response);
+      $scope.getErrMsg = "Error Status: " + response;
+    });
+  }
+
+  $scope.addTodo = function () {
+    $http.post("/addTodo", { "name": $scope.newTodo, "complete": "0" }).then(function (data, status) {
+      $scope.newTodo = "";
+      getTodos();
+    })
+  }
+
+  $scope.completeTodo = function (todo) {
+
+    $http.put("/completeTodo/" + todo.id).then(function (data, status) {
+      getTodos();
+    })
+  }
+
+  $scope.removeItem = function (todo) {
+    $http.delete("/deleteTodo/" + todo.id).then(function (data, status) {
+      getTodos();
+    })
+  }
+
+  getTodos();
+
+  $scope.$watch(function () {
+    return $scope.checkbox;
+  }, function () {
+    $scope.checkbox = Number($scope.checkbox);
+    console.log($scope.checkbox, typeof $scope.checkbox);
+  }, true);
+
+});
+
+myApp.directive("todoListView", function () {
+  return {
+    restrict: 'AECM',
+    templateUrl: '/app/directives/todoList.html',
+    replace: true,
+    scope: {
+        todoList: "=",
+        errorMessage: "@"
     }
-
-    $scope.addTodo = function () {
-      $http.post("/addTodo", { "name": $scope.newTodo, "complete": "0" }).then(function (data, status) {
-        $scope.newTodo = "";
-        getTodos();
-      })
-    }
-
-    $scope.completeTodo = function (todo) {
-
-      $http.put("/completeTodo/"+todo.id).then(function (data, status) {
-        getTodos();
-      })
-    }
-
-    $scope.removeItem = function (todo) {
-      $http.delete("/deleteTodo/"+todo.id).then(function (data, status) {
-        getTodos();
-      })
-    }
-
-    getTodos();
-
-    $scope.$watch(function(){
-      return $scope.checkbox;
-  }, function(){
-      $scope.checkbox = Number($scope.checkbox);
-      console.log($scope.checkbox, typeof $scope.checkbox);
-  },true);
-
-  });
+  }
+});
 
